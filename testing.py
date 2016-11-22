dc21cc61430e709b4271359d2cb38f822968c58d
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-11-22 15:53:46 +0000

    Added testing.py for basic test suite

---
+++ testing.py
@@ -0,0 +1,18 @@
+import objects
+import system
+import matplotlib.pyplot as plt
+
+
+def test():
+    global ball1, ball2, sys, fig, ax
+    reload(objects)
+    reload(system)
+    ball1 = objects.Ball()
+    ball2 = objects.Ball(pos=[-4, 0, 0], vel=[1, 0, 0])
+    cont = objects.Container(12)
+    sys = system.System([ball1, ball2], cont)
+    fig = plt.figure()
+    ax = plt.axes(xlim=(-20, 20), ylim=(-20, 20))
+    ax.axes.set_aspect('equal')
+    sys.init_figure(ax)
+    plt.show()
