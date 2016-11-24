624bc3d7359ca0e289d17ea49480408527b9cec2
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-11-24 01:34:58 +0000

    Expanded test suite and added if __name__ == __main__ for easy print to file of log tracebacks

--- testing.py
+++ testing.py
@@ -4,19 +4,38 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-def test():
-    global ball1, ball2, sys, fig, ax
+def mainTest():
+    global ball1, ball2, mySys, fig, ax
     reload(objects)
     reload(system)
     ball1 = objects.Ball()
     ball2 = objects.Ball(pos=[-4, 0, 0], vel=[1, 0, 0])
     cont = objects.Container(12)
-    sys = system.System([ball1, ball2], cont)
+    mySys = system.System([ball1, ball2], cont)
     fig = plt.figure()
     ax = plt.axes(xlim=(-20, 20), ylim=(-20, 20))
     ax.axes.set_aspect('equal')
     anim = animation.FuncAnimation(
-        fig, sys.next_frame, init_func=lambda: sys.init_figure(ax),
+        fig, mySys.next_frame, init_func=lambda: mySys.init_figure(ax),
         interval=20, blit=True
     )
     plt.show()
+
+
+def subTest():
+    global ball1, ball2, mySys, fig, ax
+    reload(objects)
+    reload(system)
+    ball1 = objects.Ball()
+    ball2 = objects.Ball(pos=[-4, 0, 0], vel=[1, 0, 0])
+    cont = objects.Container(12)
+    mySys = system.System([ball1, ball2], cont)
+    fig = plt.figure()
+    ax = plt.axes(xlim=(-20, 20), ylim=(-20, 20))
+    ax.axes.set_aspect('equal')
+    mySys.init_figure(ax)
+    plt.show()
+
+
+if __name__ == '__main__':
+    mainTest()
