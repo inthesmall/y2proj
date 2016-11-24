94c77d93d55300e22772f4d2dbc700c610defe5d
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-11-24 14:00:26 +0000

    added core.py to include core functionality needed by all modules

---
+++ core.py
@@ -0,0 +1,13 @@
+"""SOME DOCSTRING"""
+import numpy as _np
+
+
+FRAMERATE = 50.
+
+
+def close(float1, float2=0.):
+    """Determine if two floats are close enough to be equal. Return bool."""
+    if abs(float1 - float2) <= (100. * _np.finfo(float).eps):
+        return True
+    else:
+        return False
