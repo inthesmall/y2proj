d94c991fb510f2ed51cb329465efdaa4b0cb2c93
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-11-30 23:28:40 +0000

    made Container inherit from Ball

--- objects.py
+++ objects.py
@@ -76,6 +76,10 @@ class Ball:
         self._vel = _np.array(vel)
         self._patch = _plt.Circle(self._pos[:-1], self._radius)
 
+    def __repr__(self):
+        return ("""Ball(mass={0._mass}, radius={0._radius}, pos={0._pos},\
+ vel={0._vel})""".format(self))
+
     def getPos(self):
         """
         Return position as numpy array with 3 components [x, y, z]
@@ -132,7 +136,6 @@ class Ball:
         self.setPos(new_pos)
 
     def time_to_collision(self, other):
-        # @todo
         r1 = self.getPos()
         core.logging.debug("r1 {}".format(r1))
         v1 = self.getVel()
@@ -156,7 +159,7 @@ class Ball:
         if _np.imag(dt1) != 0:
             return None
         minimum = min(dt1, dt2)
-        if minimum > 0 and not close(minimum,0):
+        if minimum > 0 and not close(minimum, 0):
             return float(minimum)
         maximum = max(dt1, dt2)
         if maximum > 0:
@@ -164,48 +167,38 @@ class Ball:
         else:
             return None
 
-    def collide(self, other, call=True):
-        """Currently only works for Ball - Ball collisions.
-        @todo: implement Container - Ball collisions
+    def collide(self, other):
+        """
         """
         Pos = self.getPos()
         Vel = self.getVel()
         oMass = other.getMass()
-        if oMass < 0:
-            # Collided with container
-            r_norm = Pos / _np.sqrt(_np.dot(Pos, Pos))
-            u_perp = _np.dot(Vel, r_norm) * r_norm
-            v_para = Vel - u_perp
-            v_perp = -u_perp
-            v = v_perp + v_para
-            self.setVel(v)
-        else:
-            oPos = other.getPos()
-            oVel = other.getVel()
+        oPos = other.getPos()
+        oVel = other.getVel()
+        Mass = self.getMass()
 
-            Mass = self.getMass()
-            r = oPos - Pos
-            r = r / _np.sqrt(_np.dot(r, r))
-            u1_perp = _np.dot(Vel, r) * r
-            u2_perp = _np.dot(oVel, -r) * -r
-            v1_para = Vel - u1_perp
-            v2_para = oVel - u2_perp
-            v1_perp = (((u1_perp * (Mass - oMass) + (2 * oMass * u2_perp))) /
-                       (Mass + oMass))
-            v2_perp = (((2 * Mass * u1_perp) + (u2_perp * (oMass - Mass))) /
-                       (Mass + oMass))
-            v1 = v1_perp + v1_para
-            v2 = v2_perp + v2_para
-            self.setVel(v1)
-            other.setVel(v2)
+        r = oPos - Pos
+        r = r / _np.sqrt(_np.dot(r, r))
+        u1_perp = _np.dot(Vel, r) * r
+        u2_perp = _np.dot(oVel, -r) * -r
+        v1_para = Vel - u1_perp
+        v2_para = oVel - u2_perp
+        v1_perp = (((u1_perp * (Mass - oMass) + (2 * oMass * u2_perp))) /
+                   (Mass + oMass))
+        v2_perp = (((2 * Mass * u1_perp) + (u2_perp * (oMass - Mass))) /
+                   (Mass + oMass))
+        v1 = v1_perp + v1_para
+        v2 = v2_perp + v2_para
+        self.setVel(v1)
+        other.setVel(v2)
 
 
-class Container:
+class oldContainer:
     """
     Spherical container for objects of type Ball. Has infinite mass.
     """
 
-    def __init__(self, radius):
+    def __init__(self, radius, mass):
         """Initialise the container with parameters
 
         Args:
@@ -219,17 +212,28 @@ class Container:
             )
         if radius <= 0:
             raise ValueError("radius is {}, should be positive".format(radius))
+        if type(mass) not in (int, float):
+            raise TypeError(
+                "mass is type {}, should be int or float".format(
+                    type(mass)
+                )
+            )
+        if mass <= 0:
+            raise ValueError("mass is {}, should be positive".format(mass))
 
         self._radius = float(radius)
         self._patch = _plt.Circle((0, 0), self._radius, fill=False)
+        self._pos = _np.array([0, 0, 0])
+        self._vel = _np.array([0, 0, 0])
+        self._mass = mass
 
     def getPos(self):
         """Return zero vector for use with collisions"""
-        return _np.array([0, 0, 0])
+        return self._pos
 
     def getVel(self):
         """Return zero vector for use with collisions"""
-        return _np.array([0, 0, 0])
+        return self._vel
 
     def getRadius(self):
         """Return negative radius as float.
@@ -245,4 +249,19 @@ class Container:
 
     def getMass(self):
         """Return -1 for Ball.collide()"""
-        return -1
+        return self._mass
+
+
+class Container(Ball):
+    """"""
+
+    def __init__(self, radius=12, mass=2):
+        Ball.__init__(
+            self, radius=radius, mass=mass, pos=[0, 0, 0], vel=[0, 0, 0]
+        )
+        self._patch = _plt.Circle((0, 0), self._radius, fill=False)
+
+    def getRadius(self):
+        """Overrides Ball to return negative radius for collisions."""
+        return -self._radius
+
