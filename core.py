76f40200d2d1bbee60e9f559a1032ea5a520eac1
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-12-09 01:43:18 +0000

    Added array comparison to close

--- core.py
+++ core.py
@@ -11,14 +11,23 @@ logging.basicConfig(filename=time.strftime("%Y%m%d-%H%M%S") + ".log",
                     )
 
 FRAMERATE = 50.
+Kb = 1.38064852E-23
+MASS = 1.0E-26
 
 
 def close(float1, float2=0.):
     """Determine if two floats are close enough to be equal. Return bool."""
-    if abs(float1 - float2) <= 0.000001:
-        return True
-    else:
-        return False
+    if type(float1) in (int, float) and type(float2) in (int, float):
+        if abs(float1 - float2) <= 0.000001:
+            return True
+        else:
+            return False
+    elif (type(float1) in (_np.array, _np.ndarray) and
+          type(float2) in (_np.array, _np.ndarray)):
+        if abs(float1 - float2).all() <= 0.000001:
+            return True
+        else:
+            return False
 
 
 def distributeVelocities(n, v, dim):
@@ -52,8 +61,7 @@ def distributeBalls(n, radius, ballsize=1, v=8., dim=2):
     """Arranges balls in a uniform grid with randomly distributed velocities
 
     Sum of velocities is zero.
-    *v* is charcteristic velocity, not maximum due to implementation
-    contraint.
+    *v* is characteristic velocity, approximately equal to v_max/sqrt(3)
     *dim* is number of dimensions for balls to have vel components in.
     2 for animation, 3 generally.
     """
