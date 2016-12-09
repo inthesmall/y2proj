e5c1fcc61fcf3db0c3652eb8087dbe13c9eca8c6
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-12-09 01:45:58 +0000

    Added lots of new tests for the physics functionality

--- testing.py
+++ testing.py
@@ -41,18 +41,13 @@ def subTest():
 
 
 def genTest(n=6):
-    global mySys, fig, ax
+    global mySys
     reload(objects)
     reload(system)
     reload(core)
     balls = core.distributeBalls(n, 12)
     cont = objects.Container(12.)
     mySys = system.System(balls, cont)
-    fig = plt.figure()
-    ax = plt.axes(xlim=(-20, 20), ylim=(-20, 20))
-    ax.axes.set_aspect('equal')
-    mySys.init_system(ax)
-    plt.show()
 
 
 def animGenTest(n=6, r=12., ballsize=1.):
@@ -74,6 +69,67 @@ def animGenTest(n=6, r=12., ballsize=1.):
     plt.show()
 
 
+def physicsTest(num_balls=200, ballsize=0.01):
+    global mySys, pressure
+    sys.setrecursionlimit(10000)
+    reload(objects)
+    reload(system)
+    reload(core)
+    balls = core.distributeBalls(
+        n=num_balls, radius=12, ballsize=ballsize)
+    cont = objects.Container(12)
+    mySys = system.System(balls, cont)
+    mySys.init_system(None)
+    mySys.check_collide(5)
+    P = mySys.pressure(20)
+    print "pressure", P
+    print "temp", mySys.temperature()
+    core.logging.log(50, "temp is {}".format(mySys.temperature()))
+    a = P * (4. / 3.) * core._np.pi * 12.**3
+    b = num_balls * core.Kb * mySys.temperature()
+    c = P * (((4. / 3.) * core._np.pi * 12.**3)
+             - ((16. / 3.) * core._np.pi * ballsize**3 * num_balls)
+             )
+    print "Pv", a
+    print "nkT", b
+    print "frac", a / b
+    print "P(V-bn)", c
+    print "frac", c / b
+
+
+def timingTest():
+    for i in range(20, 200, 20):
+        core.logging.log(20, "time is {}".format(core.time.strftime("%H%M%S")))
+        core.logging.log(20, "i is {}".format(i))
+        physicsTest(i)
+    core.logging.log(20, "time is {}".format(core.time.strftime("%H%M%S")))
+
+
+def conservationTest(step=5):
+    global mySys
+    sys.setrecursionlimit(10000)
+    reload(objects)
+    reload(system)
+    reload(core)
+    balls = core.distributeBalls(20, 12, ballsize=0.1)
+    cont = objects.Container(12)
+    mySys = system.System(balls, cont)
+    mySys.init_system(None)
+    E0 = mySys.total_KE()
+    p0 = mySys.get_total_momentum()
+    mySys.check_collide(step)
+    E1 = mySys.total_KE()
+    p1 = mySys.get_total_momentum()
+    if core.close(p0, p1):
+        core.logging.log(20, "Momentum conserved")
+    else:
+        core.logging.log(50, "Momentum not conserved")
+
+
+def genPV():
+    None
+
+
 def run(args):
     """Run the requested test function"""
     if args[1] == "m":
@@ -82,6 +138,12 @@ def run(args):
         animGenTest(4, ballsize=1.)
     elif args[1] == "a":
         animGenTest(16, ballsize=0.25)
+    elif args[1] == "p":
+        physicsTest(num_balls=40)
+    elif args[1] == "t":
+        timingTest()
+    elif args[1] == "c":
+        conservationTest(5)
 
 
 if __name__ == '__main__':
