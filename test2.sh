0448031ba5ff46a3c0e8ed88ce3c4b4eed2ff4bd
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-12-09 01:46:26 +0000

    Updated to reflect new tests

--- test2.sh
+++ test2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while getopts :masd: testvar; do
+while getopts :masptcd: testvar; do
 	case $testvar in
 		m)
 			echo "run main"
@@ -17,7 +17,18 @@ while getopts :masd: testvar; do
 			echo "debug level $OPTARG"
 			debug=$OPTARG
 			d=true
-
+			;;
+		p)
+			echo "run physics"
+			p=true
+			;;
+		t)
+			echo "run timing test"
+			t=true
+			;;
+		c)
+			echo "run conservation test"
+			c=true
 			;;
 		\?)
 			echo "Invalid option -$OPTARG" 1>&2
@@ -47,6 +58,30 @@ elif [ "$s" = true ]; then
 	else
 		echo "ran small with no debug"
 		python testing.py s >> test.$(date +%Y%m%d%H%M).log 2>&1
+	fi
+elif [ "$p" = true ]; then
+		if [ "$d" = true ]; then
+		echo "ran physics with debug $debug"
+		python testing.py p d $debug >> test.$(date +%Y%m%d%H%M).log 2>&1
+	else
+		echo "ran physics with no debug"
+		python testing.py p >> test.$(date +%Y%m%d%H%M).log 2>&1
+	fi
+elif [ "$t" = true ]; then
+	if [ "$d" = true ]; then
+		echo "ran timing with debug $debug"
+		python testing.py t d $debug >> test.$(date +%Y%m%d%H%M).log 2>&1
+	else
+		echo "ran timing with no debug"
+		python testing.py t >> test.$(date +%Y%m%d%H%M).log 2>&1
+	fi
+elif [ "$c" = true ]; then
+	if [ "$d" = true ]; then
+		echo "ran conservation with debug $debug"
+		python testing.py c d $debug >> test.$(date +%Y%m%d%H%M).log 2>&1
+	else
+		echo "ran conservation with no debug"
+		python testing.py c >> test.$(date +%Y%m%d%H%M).log 2>&1
 	fi
 elif [ "$d" = true ]; then
 	echo "ran with debug $debug"
