9b5194043788c8fabba272d74d50a0d27193dfd8
Ethan Mills <ethan.mills@hotmail.co.uk>
2016-12-10 12:08:14 +0000

    removed functions moved to objects

--- core.py
+++ core.py
@@ -1,14 +1,22 @@
-"""SOME DOCSTRING"""
-import numpy as _np
-import numpy.random as _rand
+"""Helper functions and logging
+
+Defines:
+    FRAMERATE, Int, Constant
+    Kb, float, Constant, Boltzmann Constant
+    MASS, float, Constant, scale factor for mass
+    close(float1, float2=0), function,
+        test for equality of floats and arrays
+"""
 import logging
 import time
 
-import objects
+import numpy as _np
+
 
 logging.basicConfig(filename=time.strftime("%Y%m%d-%H%M%S") + ".log",
                     level=logging.DEBUG
                     )
+
 
 FRAMERATE = 50.
 Kb = 1.38064852E-23
@@ -28,79 +36,3 @@ def close(float1, float2=0.):
             return True
         else:
             return False
-
-
-def distributeVelocities(n, v, dim):
-    """
-    Generate *n* velocities with zero sum, characterised by *v*
-
-    Velocity components are generated in *dim* dimensions. *v* is
-    characteristic scale of velocities, not maximum. Return velocities
-    as three numpy.array
-    """
-    vx = 2 * v * (_rand.random(n) - 0.5)
-    vy = 2 * v * (_rand.random(n) - 0.5)
-    if dim == 3:
-        vz = 2 * v * (_rand.random(n) - 0.5)
-        vz -= _np.sum(vz) / n
-        # Account for rounding error
-        vz[0] -= _np.sum(vz)
-    else:
-        vz = _np.zeros(n)
-    vx -= _np.sum(vx) / n
-    vx[0] -= _np.sum(vx)
-    vy -= _np.sum(vy) / n
-    vy[0] -= _np.sum(vy)
-    logging.debug("vx {}".format(vx))
-    logging.debug("vy {}".format(vy))
-    logging.debug("vz {}".format(vz))
-    return vx, vy, vz
-
-
-def distributeBalls(n, radius, ballsize=1, v=8., dim=2):
-    """Arranges balls in a uniform grid with randomly distributed velocities
-
-    Sum of velocities is zero.
-    *v* is characteristic velocity, approximately equal to v_max/sqrt(3)
-    *dim* is number of dimensions for balls to have vel components in.
-    2 for animation, 3 generally.
-    """
-    if type(n) not in (int, float):
-        raise TypeError("n is type {}, should be int".format(type(n)))
-    if n <= 0:
-        raise ValueError("n has value {}, should be positive".format(n))
-    if n % 1 != 0:
-        raise ValueError("n is {}, should be an integer".format(n))
-    if type(radius) not in (int, float):
-        raise TypeError("radius has type {}, should be float")
-    if type(ballsize) not in (int, float):
-        raise TypeError(
-            "ballsize has type {}, should be float".format(type(ballsize))
-        )
-    if ballsize <= 0:
-        raise ValueError("ballsize is {}, should be positive".format(ballsize))
-    # Container objects return a negative radius for collisions, we need
-    # this to be positive
-    radius = _np.abs(float(radius))
-    side_len = _np.sqrt(2) * radius
-    per_row = int(_np.ceil(_np.sqrt(n)))
-    if 2 * ballsize * per_row >= side_len:
-        raise ValueError("Too many balls. Got {}".format(n))
-    vx, vy, vz = distributeVelocities(n=n, v=v, dim=dim)
-    balls = []
-    # number of balls already created
-    ball = 0
-    ballspace = side_len / per_row
-    for row in xrange(per_row):
-        for col in xrange(per_row):
-            if ball < n:
-                x = ((col + 0.5) * ballspace) - (side_len / 2)
-                y = ((row + 0.5) * ballspace) - (side_len / 2)
-                balls.append(objects.Ball(pos=[x, y, 0],
-                                          vel=[vx[ball], vy[ball], vz[ball]],
-                                          radius=ballsize
-                                          ))
-                ball += 1
-            else:
-                break
-    return balls
